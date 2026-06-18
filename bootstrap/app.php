d0951ec766d86b163ee8721004985c7c05c7b93b
Evandro <professorevandroaurelio@gmail.com>
2026-06-18 14:54:21 -0300

    Envio de email

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +10,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Confia nos headers do proxy (X-Forwarded-*) para manter esquema/host corretos.
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
